7a86c48925b8d84f09be083c4ac943731823a93c
Nabi <nabi.usonov@gmail.com>
2024-11-18 00:12:27 +0500

    Added custom ObjectId pydantic validation

--- app/schemas/utils_schema.py
+++ app/schemas/utils_schema.py
@@ -1,13 +1,22 @@
 from bson import ObjectId
-from typing import Annotated
-from fastapi import Depends
 
 
-def validate_object_id(object_id: str):
-    if not ObjectId.is_valid(object_id):
-        raise ValueError('Invalid ObjectId')
-    return object_id
+class PyObjectId(ObjectId):
 
-PyObjectId = Annotated[str, Depends(validate_object_id)]
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
 
+    @classmethod
+    def validate(cls, v, field):
+        if not ObjectId.is_valid(v):
+            raise ValueError(f"Invalid ObjectId: {v}")
+        return ObjectId(v)
 
+    @classmethod
+    def __get_pydantic_json_schema__(cls, schema, model_field) -> dict:
+        """
+        Generates a custom json schema for ObjectId,
+        telling Pydantic that the type is string with UUID format.
+        """
+        return {"type": "string", "format": "uuid"}

d056058a155ecaf4277c8d65d8bd1505ecab27e8
Nabi <nabi.usonov@gmail.com>
2024-11-17 23:22:17 +0500

    Added schema for input and output, also renamed id to object_id bc id is built-in python function

--- app/schemas/trackedtime_schemas.py
+++ app/schemas/trackedtime_schemas.py
@@ -6,11 +6,8 @@ from .user_schemas import User
 from typing import Dict
 
 
-class TrackedTime(BaseModel):
-    id: PyObjectId = Field(default_factory=PyObjectId, alias='_id')
-    user_id: PyObjectId = Field(
-        ..., description='User id related to this tracked time')
-    created_at: datetime = Field(default_factory=lambda: datetime.now())
+class TrackedTimeBase(BaseModel):
+    object_id: PyObjectId = Field(default_factory=PyObjectId, alias='_id')
     apps: Dict[str, int] = Field(
         description='Key is app name, value is time tracked in seconds.')
 
@@ -18,6 +15,27 @@ class TrackedTime(BaseModel):
         populate_by_name = True
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
+
+
+class TrackedTimeIn(TrackedTimeBase):
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "apps": {
+                    "app1": 123123,
+                    "app2": 123213
+                }
+            }
+        }
+
+
+class TrackedTimeOut(TrackedTimeBase):
+    user_id: PyObjectId = Field(
+        default='None', description='User id related to this tracked time')
+    created_at: datetime = Field(default_factory=lambda: datetime.now())
+
+    class Config:
         json_schema_extra = {
             "example": {
                 "user_id": "60f7c2d9fc13ae1b3e000000",

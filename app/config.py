df5115a2f0c6cefd71af982c44d6e1825ba623a1
Nabi <nabi.usonov@gmail.com>
2024-11-17 20:44:19 +0500

    Minor changes to files

--- app/config.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv, find_dotenv
-from os import environ
-load_dotenv(find_dotenv())
-
-
-MONGODB_URL = environ['MONGODB_URL']
-MONGODB_NAME = environ['MONGODB_NAME']
--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-
+from api.v1.router import main_router
 
 
 app = FastAPI()
+app.include_router(main_router)

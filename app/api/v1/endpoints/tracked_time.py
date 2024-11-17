778922974194a89c3d9eb12725e0ac8dcbb59bc6
Nabi <nabi.usonov@gmail.com>
2024-11-18 00:16:10 +0500

    Added endpoints for create and get_by_id

--- app/api/v1/endpoints/tracked_time.py
+++ app/api/v1/endpoints/tracked_time.py
@@ -1,26 +1,30 @@
 from fastapi import APIRouter, Depends
+from fastapi.exceptions import HTTPException
 
 from core.dependencies import RepositoryProvider
 from repos.trackedtime_repo import TrackedTimeRepository
 from schemas.utils_schema import PyObjectId
-from schemas.trackedtime_schemas import TrackedTime
+from schemas.trackedtime_schemas import TrackedTimeIn, TrackedTimeOut
 
 trackedtime_repo = RepositoryProvider('trackedtime').get_repository
 
 router = APIRouter()
 
-@router.get('/{object_id}', response_model=TrackedTime)
-async def get_tracked_time(object_id: PyObjectId, repository: TrackedTimeRepository = Depends(trackedtime_repo)):
-    try:
-        user = await repository.get_by_id(object_id)
-        return user
-    except Exception as e:
-        pass # Add some exception handling
-    
-@router.post('/', response_model=TrackedTime)
-async def create_tracked_time(object_id: PyObjectId, repository: TrackedTimeRepository = Depends(trackedtime_repo)):
-    try:
-        user = await repository.get_by_id(object_id)
-        return user
-    except Exception as e:
-        pass # Add some exception handling
+
+@router.get('/{object_id}', response_model=TrackedTimeOut)
+async def get_tracked_time(
+    object_id: PyObjectId,
+    repository: TrackedTimeRepository = Depends(trackedtime_repo)):
+
+    tracked_time = await repository.get_by_id(object_id)
+    if not tracked_time:
+        raise HTTPException(status_code=404, detail="Tracked time not found")
+    return tracked_time
+
+
+@router.post('/', response_model=TrackedTimeOut)
+async def create_tracked_time(
+    data: TrackedTimeIn,
+    repository: TrackedTimeRepository = Depends(trackedtime_repo)):
+    tracked_time = await repository.create(data=data)
+    return tracked_time

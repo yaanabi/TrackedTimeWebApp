d5f2603ad4dc541bd11ec1122f05ba2f0e026eea
Nabi <nabi.usonov@gmail.com>
2024-11-21 20:54:58 +0500

    Made client class attr to be private and made client class some type of singleton

--- app/db/mongodb.py
+++ app/db/mongodb.py
@@ -4,22 +4,29 @@ from core.config import MONGODB_URL
 
 # Maybe rewrite to be compatible with other db's?? But I don't think i will use any other DB so I just leave it as it is rn
 class MongoClient:
-    _client: AsyncIOMotorClient = None
+    __client: AsyncIOMotorClient = None
+    __instance = None
+    
+    # Don't really need instances, bc im using class methods, but just in case added it 
+    def __new__(cls, *args, **kwargs): 
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
 
     @classmethod
     def get_client(cls):
-        if cls._client is None:
-            cls._client = AsyncIOMotorClient(
+        if cls.__client is None:
+            cls.__client = AsyncIOMotorClient(
                 MONGODB_URL
             )  # Getting client doesn't involve async, so no need to await and async def(probably?)
             print('MongoDB client connected')
-        return cls._client
+        return cls.__client
 
     @classmethod
     def close_client(cls):
-        if cls._client is not None:
-            cls._client.close()
-            cls._client = None
+        if cls.__client is not None:
+            cls.__client.close()
+            cls.__client = None
             print('MongoDB client disconnected')
 
 
